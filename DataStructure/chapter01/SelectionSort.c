ddd3bb8eed05ea13ce73f3f6a534fb84b409de53
yhames <wjddn494@naver.com>
2023-08-11 15:08:41 +0900

    자료구조 - 1장 : 시간복잡도

---
+++ DataStructure/chapter01/SelectionSort.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int selectionSort(int* arr, int arr_size)
+{
+	int i, j, min, tmp;
+	int count;
+
+	for (i = 0; i < arr_size - 1; i++)
+	{
+		min = i;
+		for (j = i + 1; j < arr_size; j++)
+		{
+			if(arr[j] < arr[min])
+			{
+				count++;
+				min = j;
+			}
+		}
+		tmp = arr[min];
+		arr[min] = arr[i];
+		arr[i] = tmp;
+	}
+	return count;
+}
+
+void init(int* arr, int arr_size)
+{
+	int i;
+
+	srand(clock());
+	for (i = 0; i < arr_size; i++)
+	{
+		arr[i] = rand();
+	}
+}
+
+int main(int argc, char** argv)
+{
+	if(argc < 2)
+	{
+		printf("./a.out [number of iterations]\n");
+		exit(0);
+	}
+	const int iter = atoi(argv[1]);
+
+	int i, j;
+	int avg, expected;
+	long count;
+	
+	for (i=10; i<100; i+=10)
+	{
+		count = 0;
+		for (j=0; j < iter; j++)
+		{
+			int arr[i];
+			init(arr, i);
+			count += selectionSort(arr, i);
+		}
+		avg = count / iter;
+		expected = (i * (i-1)) / 2;
+		printf("N=%d time-complextiry : expected = %d, actual = %d\n", i, expected, avg);
+	}
+
+	return 0;
+}
